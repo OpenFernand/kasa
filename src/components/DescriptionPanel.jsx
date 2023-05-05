97759c72c391ca4b36cad56e490db51b48fa3305
Fernand Adjahossou <kafe@wanadoo.fr>
2023-05-05 22:08:05 +0200

    fin avec chevron disparaient sur page Apropos

--- src/components/DescriptionPanel.jsx
+++ src/components/DescriptionPanel.jsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import './DescriptionPanel.scss';
 
 export function DescriptionPanel(props) {
-
+// Actuel
     const [isContentVisible, setIsContentVisible] = useState(false)
-
     const showContent = () => {
         setIsContentVisible(!isContentVisible)
     }
@@ -12,10 +11,28 @@ export function DescriptionPanel(props) {
         <div className="description__panel">
         <p className="description__header">
             <span>{props.title}</span>
-            <i className="fas fa-chevron-up" onClick={showContent}></i>
+            <i className="fas fa-chevron-down" onClick={showContent}></i>
         </p>
-
         {isContentVisible && <p className="description__content" >{props.content} </p>}
         </div>
     );
+// Fin actuel
+
+/* version définitive---
+    const [isContentVisible, setIsContentVisible] = useState(false)
+    const showContent = () => {
+        setIsContentVisible(!isContentVisible)
+    const contentClass = (isContentVisible ? 'visible' : "hidden") + 'description__content'
+    const chevronClass = (isContentVisible ? 'fa-chevron-up' : "fa-chevron-down") + 'fas'
+    return (
+        <div className="description__panel">
+            <p className="description__header" onClick={showContent}>
+                <span>{props.title}</span>
+                <i className={chevronClass}></i>
+            </p>
+            <p className={contentClass}>{props.content}</p>
+        </div>
+    );
+*/
+
 }
--- src/components/ImageBanner.jsx
+++ src/components/ImageBanner.jsx
@@ -19,20 +19,41 @@ export function ImageBanner(props) {
     const newCurrentPicture = currentPicture - 1
     if (newCurrentPicture < 0) {
       setCurrentPicture(pictures.length -1)
+      return
     }
-
-    setCurrentPicture((currentPicture + 1) % pictures.length) // modulo limite dans la longueur de l' Array
+    setCurrentPicture(currentPicture - 1)
   }
 
+// Les chevrons se montrent que si pictures sont available 
+const arePicturesAvailable = () => {
+  return pictures && pictures.length > 0
+}
+
+// Pour Gérer la page "A propos" et gerer le map
+// s'il n'y a pas pas de props. picture il doit afficher simplemnt une image au hasard
+
+const getCarouselOrDefaultImage = () => {
+  if (!pictures || pictures.length === 0) {
+    return <img src = 'https://picsum.photos/800' className='show' alt='' />
+    }
+    return pictures.map((pic, i) => (
+      <img key={pic} src = {pic} alt='' className = {getClassName(i)}></img>
+    ))
+  }
   return (
     <div className='image__banner'>
-      <div className='image__container'>
-        {pictures.map((pic, i) => (
-          <img key={pic} src={pic} alt='' className={getClassName(i)} ></img>
-        ))}
+      <div className='image__container'>{getCarouselOrDefaultImage()}
       </div>
-            <button onClick={moveToNext}>Previous</button>
-            <button onClick={moveToPrevious}> Next</button>
+      {arePicturesAvailable() && ( // Les chevrons se montrent que si pictures sont available  
+        <>
+          <button className='btn btn-next' onClick={moveToNext}>
+            <i className='fas fa-chevron-left'></i>
+          </button>
+          <button className='btn btn-previous' onClick={moveToPrevious}>
+            <i className='fas fa-chevron-right'></i>
+          </button>
+        </>
+      )}
     </div>
   )
 }
